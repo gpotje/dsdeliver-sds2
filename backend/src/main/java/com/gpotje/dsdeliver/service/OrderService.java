8027ed63adb5ad2452da057492da3b3630ffc446
gpotje <gpotje.souza@gmail.com>
2021-01-07 17:31:12 -0300

    POST/orders

--- backend/src/main/java/com/gpotje/dsdeliver/service/OrderService.java
+++ backend/src/main/java/com/gpotje/dsdeliver/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.gpotje.dsdeliver.service;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,14 +9,21 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.gpotje.dsdeliver.DTO.OrderDTO;
+import com.gpotje.dsdeliver.DTO.ProductDTO;
 import com.gpotje.dsdeliver.entities.Order;
+import com.gpotje.dsdeliver.entities.OrderStatus;
+import com.gpotje.dsdeliver.entities.Product;
 import com.gpotje.dsdeliver.repository.OrderRepository;
+import com.gpotje.dsdeliver.repository.ProductRepository;
 
 @Service
 public class OrderService {
 
 		@Autowired
 	private OrderRepository orderRepository;
+		
+		@Autowired
+	private ProductRepository productRepository;
 	
 	@Transactional(readOnly = true)
 	public List<OrderDTO>findAll(){
@@ -25,6 +33,35 @@ public class OrderService {
 		
 		
 	}
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto){
+	 Order order = new Order(null,
+			 dto.getAddress(),
+			 dto.getLatitude(),
+			 dto.getLongitude(),
+			 Instant.now(),
+			 OrderStatus.PENDING);
+	 
+	 //percorre todos os produtos associados
+	 for(ProductDTO p : dto.getProduct()) {
+		 Product product = productRepository.getOne(p.getId());
+		 order.getProducts().add(product);
+	 }
+	 order = orderRepository.save(order);
+	 return new OrderDTO(order);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
 	
 	
 }
