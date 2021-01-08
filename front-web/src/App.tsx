14c74cffad951f03c337b7291012b288c381a8cf
gpotje <gpotje.souza@gmail.com>
2021-01-08 15:13:35 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
-import Hello from './Hello';
+import Home from './Home';
 import Navbar from './Navbar';
 
 function App() {
   return (
     <div >
         <Navbar/>
+        <Home/>
     </div>
   );
 }
