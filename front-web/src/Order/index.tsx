275a6526f5050e1c2339fd48e97d4fa7e94d7dd9
gpotje <gpotje.souza@gmail.com>
2021-01-08 21:46:27 -0300

    Part 1 completa

--- front-web/src/Order/index.tsx
+++ front-web/src/Order/index.tsx
@@ -1,13 +1,33 @@
+import { useEffect, useState } from 'react';
+import { fetchProducts } from './api';
+import ProductsList from './ProductsList';
 import StepsHeader from './StepsHeader';
 import './styles.css';
+import { Product } from './types';
 
 
 
 function Orders(){
+        const [product,setProducts] = useState<Product[]>([]);
+
+        
+
+        useEffect(()=>{
+            fetchProducts().
+            then(response => setProducts(response.data)).
+            catch(error =>console.log(error))    
+        },[]);
+
+
+
+
         return(
                 <div className="orders-container">
                    <StepsHeader/>
+                   <ProductsList products={product}/>
+                   
                </div>
+
             
         )
 
